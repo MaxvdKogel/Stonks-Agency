766c48ade75135be8e076b364f1ae2cf92ed74c8
Maxvdkogel <maxvdkogel123@gmail.com>
2021-06-09 23:42:56 +0200

    mobile hero and cases

---
+++ app.js
@@ -0,0 +1,15 @@
+var sleepingCat = bodymovin.loadAnimation({
+    container: document.querySelector('.sleeping-cat'),
+    renderer: 'svg',
+    loop: true,
+    autoplay: true,
+    path: 'lottie-animations/slapende-kat-final-2.json'
+})
+
+var standingCat = bodymovin.loadAnimation({
+    container: document.querySelector('.standing-cat'),
+    renderer: 'svg',
+    loop: true,
+    autoplay: true,
+    path: 'lottie-animations/Kat staand.json'
+})
