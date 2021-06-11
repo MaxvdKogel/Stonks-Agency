c8cd8727b3c9b4bb00a435efca8798713870c8f9
Maxvdkogel <maxvdkogel123@gmail.com>
2021-06-11 14:36:49 +0200

    added about us and contact page for mobile

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
-var sleepingCat = bodymovin.loadAnimation({
+// ------------ lottie animations ------------------
+
+const sleepingCat = bodymovin.loadAnimation({
     container: document.querySelector('.sleeping-cat'),
     renderer: 'svg',
     loop: true,
@@ -6,10 +8,18 @@ var sleepingCat = bodymovin.loadAnimation({
     path: 'lottie-animations/slapende-kat-final-2.json'
 })
 
-var standingCat = bodymovin.loadAnimation({
+const standingCat = bodymovin.loadAnimation({
     container: document.querySelector('.standing-cat'),
     renderer: 'svg',
     loop: true,
     autoplay: true,
     path: 'lottie-animations/Kat staand.json'
+})
+
+const catLicking = bodymovin.loadAnimation({
+    container: document.querySelector('.cat-licking'),
+    renderer: 'svg',
+    loop: true,
+    autoplay: true,
+    path: 'lottie-animations/kat pootje.json'
 })
