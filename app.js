8f058a58f5bc04203b7fda93eadea68bd68f62d5
Maxvdkogel <maxvdkogel123@gmail.com>
2021-06-15 00:16:49 +0200

    added projects hover

--- app.js
+++ app.js
@@ -7,20 +7,81 @@ var cursor = document.querySelector(".cursor");
 
 // ------------------- hovers ---------------------
 
-const ctas = document.querySelector(".cta");
-const cta_span = document.querySelector(".cta__span");
+// hero cta
 
-ctas.addEventListener("mouseover", e => {
-    cta_span.style.left = e.pageX - ctas.offsetLeft + "px";
-    cta_span.style.top = e.pageY - ctas.offsetTop + "px";
+const hero_cta = document.querySelector(".hero__cta");
+const hero_cta_span = document.querySelector(".hero__cta-span");
+
+hero_cta.addEventListener("mouseover", e => {
+    hero_cta_span.style.left = e.pageX - hero_cta.offsetLeft + "px";
+    hero_cta_span.style.top = e.pageY - hero_cta.offsetTop + "px";
     cursor.classList.add("hide-cursor");
 });
 
-ctas.addEventListener("mouseout", e => {
-    cta_span.style.left = e.pageX - ctas.offsetLeft + "px";
-    cta_span.style.top = e.pageY - ctas.offsetTop + "px";
+hero_cta.addEventListener("mouseout", e => {
+    hero_cta_span.style.left = e.pageX - hero_cta.offsetLeft + "px";
+    hero_cta_span.style.top = e.pageY - hero_cta.offsetTop + "px";
     cursor.classList.remove("hide-cursor");
 });
+
+// contact cta
+
+const contact_cta = document.querySelector('.contact__cta');
+const contact_cta_span = document.querySelector('.contact__cta-span');
+
+contact_cta.addEventListener("mouseover", e => {
+    contact_cta_span.style.left = e.pageX - contact_cta.offsetLeft + "px";
+    contact_cta_span.style.top = e.pageY - contact_cta.offsetTop + "px";
+    cursor.classList.add("hide-cursor");
+});
+
+contact_cta.addEventListener("mouseout", e => {
+    contact_cta_span.style.left = e.pageX - contact_cta.offsetLeft + "px";
+    contact_cta_span.style.top = e.pageY - contact_cta.offsetTop + "px";
+    cursor.classList.remove("hide-cursor");
+});
+
+// view case
+
+const vastgoed = document.querySelector(".desktop-vastgoed");
+const case_study = document.querySelector(".case-study");
+
+vastgoed.addEventListener('mouseover', () => {
+    cursor.classList.add("view-case");
+    case_study.classList.add("view-case__visible");
+})
+
+vastgoed.addEventListener('mouseleave', () => {
+    cursor.classList.remove("view-case");
+    case_study.classList.remove("view-case__visible");
+})
+
+// bezoek website
+
+const phoneroyal = document.querySelector(".desktop-phoneroyal");
+const bezoek_website = document.querySelector(".bezoek-website");
+
+phoneroyal.addEventListener('mouseover', () => {
+    cursor.classList.add("bezoek-website__hover");
+    bezoek_website.classList.add("bezoek-website__visible");
+})
+
+phoneroyal.addEventListener('mouseleave', () => {
+    cursor.classList.remove("bezoek-website__hover");
+    bezoek_website.classList.remove("bezoek-website__visible");
+})
+
+const jasper = document.querySelector(".desktop-jasper");
+
+jasper.addEventListener('mouseover', () => {
+    cursor.classList.add("bezoek-website__hover");
+    bezoek_website.classList.add("bezoek-website__visible");
+})
+
+jasper.addEventListener('mouseleave', () => {
+    cursor.classList.remove("bezoek-website__hover");
+    bezoek_website.classList.remove("bezoek-website__visible");
+})
 
 // ------------ lottie animations ------------------
 
